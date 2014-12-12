a0eefd33b09d88e9c7a2fa5370eeb82a149c90f9
Liza Gray <lizajgray6590@gmail.com>
2014-12-12 13:33:03 -0800

    added randomness to collection view

--- app/scripts/collection.js
+++ app/scripts/collection.js
@@ -21,7 +21,7 @@
    var $collection = $(".collection-container .row");
    $collection.empty();
  
-   for (var i = 0; i < 33; i++) {
+   for (var i = 0; i < Math.floor((Math.random() * 100) + 25); i++) {
      var $newThumbnail = buildAlbumThumbnail();
      $collection.append($newThumbnail);
    }
